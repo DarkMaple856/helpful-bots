49b724f7bc567aa10910ea5d9835367ac9cf2a8b
MapleTree856 <69950920+MapleTree856@users.noreply.github.com>
2020-10-22 13:58:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
-print("Post Exam Games")
-AMONG US BUT HAYDEN WHRECKS YALL WIF HIS BIG BWAIN
+answer = input("Are you feeling happy today?")
+if answer == "yes":
+  print("Glad to hear that! Keep pressing on!")
+if answer == "no":
+  print("Oh no! Hope you overcome your problems soon and remember to never give up!")
